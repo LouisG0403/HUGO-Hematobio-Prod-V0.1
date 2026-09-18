f92a24c928d9d3a9140637d03d85dd15858f623c
Louis0403 <louis.guillet@epitech.eu>
2026-09-18 10:58:05 +0200

    fix: update centres database schema

--- server/src/db/init.sql
+++ server/src/db/init.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS centres (
     name VARCHAR(150) NOT NULL,
     city VARCHAR(100) NOT NULL,
     structure VARCHAR(150),
-    position_x DECIMAL(5,2) NOT NULL,
-    position_y DECIMAL(5,2) NOT NULL,
+    longitude DECIMAL(9,6),
+    latitude DECIMAL(9,6),
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -14,16 +14,16 @@ INSERT INTO centres (
     name,
     city,
     structure,
-    position_x,
-    position_y,
+    longitude,
+    latitude,
     description
 )
 VALUES
-    ('CHU de Brest', 'Brest', NULL, 15.00, 16.00, NULL),
-    ('CHU de Rennes', 'Rennes', NULL, 40.00, 38.00, NULL),
-    ('CHU de Nantes', 'Nantes', NULL, 15.00, 63.00, NULL),
-    ('CHU d''Angers', 'Angers', NULL, 65.00, 45.00, NULL),
-    ('CHU du Mans', 'Le Mans', NULL, 75.00, 18.00, NULL),
-    ('CHU de Tours', 'Tours', NULL, 70.00, 72.00, NULL),
-    ('CHU de Poitiers', 'Poitiers', NULL, 35.00, 80.00, NULL),
-    ('CHU d''Orléans', 'Orléans', NULL, 48.00, 8.00, NULL);
+    ('CHU de Brest', 'Brest', NULL, -4.486100, 48.390400, NULL),
+    ('CHU de Rennes', 'Rennes', 'CRTH', -1.677800, 48.117300, NULL),
+    ('CHU de Nantes', 'Nantes', NULL, -1.553600, 47.218400, NULL),
+    ('CHU d''Angers', 'Angers', NULL, -0.563200, 47.478400, NULL),
+    ('CHU du Mans', 'Le Mans', NULL, 0.199600, 48.006100, NULL),
+    ('CHU de Tours', 'Tours', NULL, 0.689000, 47.394100, NULL),
+    ('CHU de Poitiers', 'Poitiers', NULL, 0.346700, 46.580200, NULL),
+    ('CHU d''Orléans', 'Orléans', NULL, 1.909300, 47.903000, NULL);
